bb87a83cc6bfbae721466c04e64cbc31f8c74fa2
MeBobby-33 <me.babu33@gmail.com>
2021-02-18 06:12:12 +0800

    section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'client';
+  users: any;
+
+constructor(private http: HttpClient){}
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+
+   this.getusers();
+  }
+
+// tslint:disable-next-line: typedef
+getusers(){
+  this.http.get('https://localhost:5001/api/users').subscribe(response => {
+    this.users = response;
+  // tslint:disable-next-line: no-shadowed-variable
+  }, error => {
+   console.log(error);
+  });
+}
 }
